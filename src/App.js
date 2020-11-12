bba4d9a0b166819df301f5b721efa19cb45bb71e
leandro <leandro@pop-os.localdomain>
2020-11-12 16:53:46 -0300

    init commint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+
+
 
 function App() {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
+  const getLocation = () =>{
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+
+    });
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <span>Teste</span>
+      <button onClick={getLocation}>CLIQUE AQUI</button>
+      <label>Latitude: {latitude}</label>
+      <label>Longitude:{longitude}</label>
       </header>
+      
     </div>
   );
 }
